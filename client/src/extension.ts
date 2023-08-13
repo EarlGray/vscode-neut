f71aef3e30b1d0a807b2b17e5dabdd0927818c64
Dmytro S <me@dmytrish.net>
2023-08-13 19:40:19 +0200

    vscode-neut seems to work: it runs and uses `neut lsp`!

---
+++ client/src/extension.ts
@@ -0,0 +1,38 @@
+import { ExtensionContext } from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+} from 'vscode-languageclient/node'
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    const serverOptions: ServerOptions = {
+        command: "neut",
+        args: ["lsp"]
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{
+            scheme: "file",
+            language: "neut",
+        }]
+    };
+
+    client = new LanguageClient(
+        'vscodeNeut',
+        serverOptions,
+        clientOptions,
+    );
+
+    client.start()
+        .catch((e) => { throw e });
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
